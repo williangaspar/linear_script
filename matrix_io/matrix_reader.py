fcf7076a382079f05fb4a2df62b0d53d1b0ef73f
Willian Gaspar <williangaspar360@gmail.com>
2025-09-26 22:13:26 -0300

    better way to end matrix

--- matrix_io/matrix_reader.py
+++ matrix_io/matrix_reader.py
@@ -1,7 +1,7 @@
 from sympy import Matrix
 from fractions import Fraction
 
-end_command = "END"
+end_command = "D"
 
 
 def read_matrix():
@@ -16,11 +16,18 @@ def read_matrix():
         if data.upper() != end_command:
             try:
                 row = data.split(" ")
+                is_end_line = False
+                if row[len(row) - 1].upper() == end_command:
+                    is_end_line = True
+                    row = row[:-1]
+                
                 fraction_row = list(map(Fraction, row))
 
                 if number_of_columns == 0:
                     number_of_columns = len(fraction_row)
                     matrix.append(fraction_row)
+                if is_end_line:
+                    break
                 elif number_of_columns != len(fraction_row):
                     print(
                         "Invalid input. The matrix must have the same number of columns in each row. Please try again:"
